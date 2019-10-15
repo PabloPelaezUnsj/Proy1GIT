31bdc73febe83be065e75b4f556103cbcefc80fa
Pablo Pelaez <blopapelaez10@gmail.com>
2019-10-15 15:12:33 -0300

    Initial commit

---
+++ src/Proy1GIT.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Proy1GIT is
+			port(	x1, x2: in	std_logic_vector(3 downto 0);
+					displays: out std_logic_vector(13 downto 0) );
+end Proy1GIT;
+
+
+architecture bev of Proy1GIT
+
+begin
+
+--TODO
+
+
+
+
+end bev;
+
+	
---
+++ src/adder.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity adder is
+port(	x1, x2: in	std_logic_vector(3 downto 0);
+		result: out std_logic_vector(3 downto 0);
+		carry : out std_logic_vector);
+end adder;
+
+architecture bev of adder
+
+begin
+
+--TODO
+
+
+end bev;
